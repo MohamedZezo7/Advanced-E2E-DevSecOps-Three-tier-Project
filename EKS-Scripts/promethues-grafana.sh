fc30ad45c0d40fed5196cb4fd4ef50137758e5a5
Mohamed Abdelaziz <mohamedabdelazizk10@gmail.com>
2024-02-05 18:59:20 -0800

    hamada edit

--- EKS-Scripts/promethues-grafana.sh
+++ EKS-Scripts/promethues-grafana.sh
@@ -10,22 +10,22 @@ if ./create-eks.sh && [ $? -eq 0 ]; then
    kubectl create namespace ${NAMESPACE} || true 
 
    echo " Deploy prometheus on eks "
-   helm repo add stable https://charts.helm.sh/stable
-   helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+   helm repo add ${RELEASE_NAME} https://prometheus-community.github.io/helm-charts
    helm repo update
+   helm install prometheus ${RELEASE_NAME}/kube-prometheus-stack
 
    echo " Wait Pods to Start "
    sleep 2m
 
    echo " change prometheus service to NodePort "
-   kubectl patch svc stable-kube-prometheus-sta-prometheus -n ${NAMESPACE} -p '{"spec": {"type": "LoadBalancer"}}'
-   kubectl patch svc stable-grafana -n ${NAMESPACE} -p '{"spec": {"type": "LoadBalancer"}}'
+   kubectl patch svc prometheus-kube-prometheus-prometheus -n ${NAMESPACE} -p '{"spec": {"type": "LoadBalancer"}}'
+   kubectl patch svc prometheus-grafana -n ${NAMESPACE} -p '{"spec": {"type": "LoadBalancer"}}'
    echo "--------------------Creating External-IP--------------------"
    sleep 10s
 
    echo "--------------------Prometheus & Grafana  Ex-URL--------------------"
-   kubectl get service stable-kube-prometheus-sta-prometheus -n ${NAMESPACE} | awk '{print $4}'
-   kubectl get service stable-grafana -n ${NAMESPACE} | awk '{print $4}'
+   kubectl get service prometheus-kube-prometheus-prometheus -n ${NAMESPACE} | awk '{print $4}'
+   kubectl get service prometheus-grafana -n ${NAMESPACE} | awk '{print $4}'
    
 
 else
